fcb086e90b5e77fa0875307858e8e3b86c78814b
Anna Levchenko <anna.levchenko@quadcode.com>
2024-03-26 12:30:56 +0200

    Add map gradient

--- src/pages/Main/ui/Map/styled.jsx
+++ src/pages/Main/ui/Map/styled.jsx
@@ -1,9 +1,20 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-    max-width: 980px;
-    height: 400px;
-    margin: 0 auto;
+  position: relative;
+  width: 100vw;
+  height: 400px;
+  margin: 0 auto;
+
+  ::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(90deg, #f70d29 0%, #ec03ff 100%);
+    opacity: 0.5;
+    pointer-events: none;
+  }
 `;
 
 export const Container = styled.iframe`
