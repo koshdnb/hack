ed86357da49392d3daeac16a5eb836f3af677456
Anna Levchenko <anna.levchenko@quadcode.com>
2024-03-28 13:53:52 +0200

    Add mobile version WIP

--- src/pages/Main/ui/TimeLine/index.jsx
+++ src/pages/Main/ui/TimeLine/index.jsx
@@ -1,4 +1,5 @@
 import { GradientTitle } from '../../../../shared/ui/GradientTitle';
+import { Layout } from '../../styled';
 
 import Hands from './images/hands.svg';
 import Registration from './images/registration.svg';
@@ -7,61 +8,66 @@ import Rocket from './images/rocket.svg';
 import Prize from './images/prize.svg';
 import {
   Wrapper,
-  Topline,
-  BottomLine,
-  Title,
-  Description,
-  CenterLine,
-  CenterLineItem,
-  Line,
+  TimelineWrap,
+  TimelineList,
+  TimelineItem,
+  ItemTextWrap,
+  ItemDate,
+  ItemDescription,
+  ItemImage,
 } from './styled';
+
+const events = [
+  {
+    date: 'APRIL 1',
+    description: 'Registration',
+    Icon: Registration,
+  },
+  {
+    date: 'MAY 5',
+    description: 'Registration Closes',
+    Icon: RegistrationClose,
+  },
+  {
+    date: 'MAY 15th',
+    description: 'TeamUP deadline',
+    Icon: Hands,
+  },
+  {
+    date: 'JUNE 15th',
+    description: 'HackAIthon starts',
+    Icon: Rocket,
+  },
+  {
+    date: 'JUNE16th',
+    description: 'Awards Ceremony',
+    Icon: Prize,
+  },
+];
 
 export const TimeLine = () => {
   return (
     <Wrapper>
-      <GradientTitle>the hackaithon timeline</GradientTitle>
-      <Topline>
-        <div>
-          <Title>APRIL 1</Title>
-          <Description>Registration</Description>
-        </div>
-        <div>
-          <Title>MAY 15th</Title>
-          <Description>TeamUP deadline</Description>
-        </div>
-        <div>
-          <Title>JUNE16th</Title>
-          <Description>Awards Ceremony</Description>
-        </div>
-      </Topline>
-      <CenterLine>
-        <Line />
-        <CenterLineItem>
-          <Registration />
-        </CenterLineItem>
-        <CenterLineItem>
-          <RegistrationClose />
-        </CenterLineItem>
-        <CenterLineItem>
-          <Hands />
-        </CenterLineItem>
-        <CenterLineItem>
-          <Rocket />
-        </CenterLineItem>
-        <CenterLineItem>
-          <Prize />
-        </CenterLineItem>
-      </CenterLine>
-      <BottomLine>
-        <div>
-          <Title>MAY 5</Title>
-          <Description>Registration Closes</Description>
-        </div>
-        <div>
-          <Title>JUNE 15th</Title>
-          <Description>HackAIthon starts</Description>
-        </div>
-      </BottomLine>
+      <Layout>
+        <TimelineWrap>
+          <GradientTitle>the hackaithon timeline</GradientTitle>
+
+          <TimelineList>
+            {events.map(({ date, description, Icon }, i) => {
+              return (
+                <TimelineItem key={date} index={i}>
+                  <ItemTextWrap>
+                    <ItemDate>{date}</ItemDate>
+                    <ItemDescription>{description}</ItemDescription>
+                  </ItemTextWrap>
+                  {/* <ItemImage imgUrl={`./images/${icon}.svg`} /> */}
+                  <ItemImage><Icon /></ItemImage>
+                </TimelineItem>
+              );
+            })}
+          </TimelineList>
+        </TimelineWrap>
+      </Layout>
     </Wrapper>
   );
 };
